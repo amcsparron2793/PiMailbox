dc5426fdb280d3853c0584ecaa02b50f51946d25
Andrew McSparron <Amcsparron@albanyny.gov>
2022-01-07 20:10:35 -0500

    MailMonitor functions perfectly as is, has not been tested as an imported module.

--- python_files/MailMonitor.py
+++ python_files/MailMonitor.py
@@ -7,13 +7,15 @@ Classes/functions to do the heavy lifting of waiting for new mail in AWDGISPlans
 
 # imports
 import ctypes  # for the VM_QUIT to stop PumpMessage()
+import threading
+import time
+
 import pythoncom
 import win32com.client
 import sys
 
-
 # outlook config
-SHARED_MAILBOX = "Your Mailbox Name"
+SHARED_MAILBOX = "AWDGISPlans"
 
 # get the outlook instance and inbox folder
 session = win32com.client.Dispatch("Outlook.Application").Session
@@ -22,26 +24,34 @@ shared_inbox = session.GetSharedDefaultFolder(user, 6).Items  # 6 is Inbox
 
 
 class HandlerClass(object):
-
     def OnItemAdd(self, item):
         print("New item added in shared mailbox")
         if item.Class == 43:
             print("The item is an email!")
 
 
-outlook = win32com.client.DispatchWithEvents(shared_inbox, HandlerClass)
+class WatcherThread(threading.Thread):
+    def run(self):
+        print("Starting up Outlook watcher\n"
+              "To terminate the program, press 'Ctrl + C'")
+        pythoncom.PumpMessages()
 
 
-def main():
-    print("Starting up Outlook watcher")
-    pythoncom.PumpMessages()
+def MailMonMain():
+    win32com.client.DispatchWithEvents(shared_inbox, HandlerClass)
+    WatcherThread(daemon=True).start()
+
+
+def RunMailMon():
+    status = MailMonMain()
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print("Terminating program..")
+            ctypes.windll.user32.PostQuitMessage(0)
+            sys.exit(status)
 
 
 if __name__ == "__main__":
-    try:
-        status = main()
-        sys.exit(status)
-    except KeyboardInterrupt:
-        print("Terminating program..")
-        ctypes.windll.user32.PostQuitMessage(0)
-        sys.exit()
+    RunMailMon()
