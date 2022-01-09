b22050c698f96e498282025e72a14c5b9e9d8bdb
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2022-01-08 22:44:40 -0500

    MailMonitorSMTP.py is functional, and removed some unneeded TODOs
    MailMonitorSMTP.py now logs in, and attempts to monitor for new emails. after the first iter of the While loop in NewEmailWatcher() data becomes blank and the initial comparison throws an IndexError...?

--- python_files/MailMonitorSMTP.py
+++ python_files/MailMonitorSMTP.py
@@ -3,7 +3,10 @@ MailMonitorSMTP.py
 
 Connects to AWDGISPlans using SMTP.
 The script then waits for a new message to come in.
-When that happens, the script  prints a message to the screen
+When that happens, the script  prints a message to the screen.
+
+based on https://stackoverflow.com/questions/49086465/python-keep-checking-new-email-and-alert-of-further-new-emails
+
 """
 
 # imports
@@ -13,11 +16,14 @@ import time
 from socket import error
 from sys import stderr
 
+# globals
 # make a var so that the stdout can be set back to its normal state
 org_stdout = sys.stdout
 
 
+# TODO: needs to be turned into a class, and have its error handling updated
 def mail_login(email_user):
+    """ logs into an IMAP4 email server."""
     email_pass = input('Password: ')
     try:
         mail.login(email_user, email_pass)
@@ -27,6 +33,7 @@ def mail_login(email_user):
 
 
 def NewEmailWatcher():
+    """ Watches an email inbox for new messages. """
     global mail
     global data
     global id_list
@@ -37,9 +44,6 @@ def NewEmailWatcher():
         mail = imaplib.IMAP4_SSL(host='outlook.office365.com', port=993)  # ("imap.gmail.com", 993)
         print(mail.welcome)
     except error:  # socket.error
-        # FIXME: make this log to stderr fileoutput correctly - moving all logging to its own .py file might work best?
-        #  Moving the logging to a new file should just be a matter of repointing stderr to logging.whateverfunction()
-        #  and moving functions to a new .py file?
         print('Connection could not be made due to \'' + str(sys.exc_info()[1]) + '\', please try again later')
         sys.stderr.write(str(sys.exc_info()))
         e = True
@@ -48,27 +52,40 @@ def NewEmailWatcher():
     # set email, ask for password, login and select inbox
     email_user = 'amcsparron@albanyny.gov\\AWDGISPlans'  # raw_input('Email: ')
     print('Account: ' + email_user)
+
     # mail_login is my function
     mail_login(email_user)
-    # these are two attributes of the imported mail module
-    mail.list()
-    # mail.select(mailbox='inbox')
 
-    latest_email_uid = ''
+    mail.list()
+
+    latest_email_uid = None
+
+    print("Monitoring email for new messages...")
 
     while True:
         mail.select("Inbox", readonly=True)
-        result, data = mail.uid("Search", None, "ALL")  # search and return uids instead
-        ids = data[0]  # data is a list.
-        id_list = ids.split()  # ids is a space separated string
-        print("Monitoring email for new messages...")
-        if data[0].split()[-1] == latest_email_uid:
+
+        result, data = mail.uid("Search", latest_email_uid, "ALL")  # search and return uids
+
+        """ids = data[0]  # data is a list.
+        id_list = ids.split()  # ids is a space separated string"""
+
+        if data[0].split()[-1].decode("utf-8") == latest_email_uid:
+            print(latest_email_uid)
             time.sleep(120)  # put your value here, be sure that this value is sufficient ( see @tripleee comment below)
         else:
-            result, data = mail.uid('search', latest_email_uid, "ALL")
-                                    #'(RFC822)')  # fetch the email headers and body (RFC822) for the given ID
-            raw_email = data[0][1]
-            latest_email_uid == data[0].split()[-1]
-            print("New Email Received!! ")
+            print(latest_email_uid)
+            result, data = mail.uid('Search', latest_email_uid, "ALL")
+            """
+            "ALL" can be replaced with '(RFC822)')  
+            in order to fetch the email headers and body (RFC822) for the given ID
+            """
+
+            #raw_email = data[0][1]
+
+            latest_email_uid = data[0].split()[-1].decode("utf-8")
+            print("New Email Received!! - uid is {} type is {}".format(latest_email_uid, type(latest_email_uid)))
+
             time.sleep(120)  # put your value here, be sure that this value is sufficient ( see @tripleee comment below)
 
+NewEmailWatcher()
