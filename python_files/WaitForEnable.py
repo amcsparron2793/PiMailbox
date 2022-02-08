2a3f58bc055b79ef6845597ce135e98b43a7d5b4
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2022-02-08 18:57:34 -0500

    added WaitForEnable.py, when the module is run directly, it initializes the Enable Button, and waits for a press. When that happens, it calls runs system("python3 MailMonitorSMTP.py")

---
+++ python_files/WaitForEnable.py
@@ -0,0 +1,33 @@
+#! python3
+
+"""
+WaitForEnable.py
+
+This module initializes the Enable Button and waits for a press.
+When that happens, it calls runs system("python3 MailMonitorSMTP.py").
+
+"""
+
+from time import sleep
+from os import system
+
+from gpiozero import Button
+
+enable_button = Button(25)
+
+
+# functions
+def WaitForEnable():
+    """ Checks if enable_button was pressed,
+     and calls system("python3 MailMonitorSMTP.py") if it was.
+     Otherwise, the program sleeps for 0.5 seconds."""
+
+    while True:
+        if enable_button.is_pressed:
+            system("python3 MailMonitorSMTP.py")
+        else:
+            sleep(0.5)
+
+
+if __name__ == "__main__":
+    WaitForEnable()
