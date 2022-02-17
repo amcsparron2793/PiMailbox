a8cda250d013ceab81f60a3faf96bead3d338eb1
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2022-02-16 19:50:51 -0500

    trying mail.fetch(latest_email_uid, "(BODY[HEADER.FIELDS(Subject)")

--- python_files/MailMonitorSMTP.py
+++ python_files/MailMonitorSMTP.py
@@ -113,6 +113,9 @@ def NewEmailWatcher():
         print("Running Email Check on {}.\nMost up to date UID before check is {}".format(time.strftime("%x at %X"),
                                                                                           latest_email_uid))
         # TODO: add in param for from field
+        if latest_email_uid is not None:
+            print(mail.fetch(latest_email_uid, "(BODY[HEADER.FIELDS(Subject)"))
+            # "(BODY[HEADER.FIELDS(Subject)"
 
         # this isn't used so that an index error cant be thrown if data comes back blank.
         # latest_email_uid = data[0].split()[-1].decode("utf-8")
@@ -135,6 +138,8 @@ def NewEmailWatcher():
                 latest_email_uid = data[0].split()[-1].decode("utf-8")
                 if latest_email_uid != olddata[0] and not firstrun:
                     # TODO: add in param for from field
+                    if latest_email_uid is not None:
+                        print(mail.fetch(latest_email_uid, "(BODY[HEADER.FIELDS(Subject)"))
                     # Mech.YouGotMail() turns on the mail led and raises the servo to max.
                     Mech.YouGotMail()
 
