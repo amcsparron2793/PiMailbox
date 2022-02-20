9b59f02aec6eb3341abc26c615fed73377225c5e
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2022-02-20 14:06:26 -0500

    converted VolumeControl.py into a class in MechClassesPi.py NEEDS TESTING

--- python_files/MechClassesPi.py
+++ python_files/MechClassesPi.py
@@ -18,8 +18,88 @@ from time import sleep
 
 import gpiozero
 import threading
+
+# start of low level i2c/sci modules from adafruit
 # noinspection PyUnresolvedReferences
 import Adafruit_CharLCD as LCD
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
+class VolumeControl:
+    """Allows for pot based volume control using an MCP3008 ADC Chip."""
+    def __init__(self):
+        # These are never used outside the __init__ method,
+        # so they don't need to be self.xxx
+        spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+        # create the cs (chip select) - this is for the ADC chip
+        cs = digitalio.DigitalInOut(board.D22)
+        # create the mcp object - create the chip object itself
+        mcp = MCP.MCP3008(spi, cs)
+
+        # create an analog input channel on pin 0
+        self.chan0 = AnalogIn(mcp, MCP.P0)
+
+        # works great as an init check.
+        print('Raw ADC Value: ', self.chan0.value)
+        print('ADC Voltage: ' + str(self.chan0.voltage) + 'V')
+
+        # to keep from being jittery we'll only change
+        # volume when the pot has moved a significant amount
+        # on a 16-bit ADC
+        self.tolerance = 250
+
+    @staticmethod
+    def _remap_range(value, left_min, left_max, right_min, right_max):
+        """ This remaps a value from original (left) range to new (right) range. """
+
+        # Figure out how 'wide' each range is
+        left_span = left_max - left_min
+        right_span = right_max - right_min
+
+        # Convert the left range into a 0-1 range (int)
+        value_scaled = int(value - left_min) / int(left_span)
+
+        # Convert the 0-1 range into a value in the right range.
+        return int(right_min + (value_scaled * right_span))
+
+    def WatchVol(self):
+        """ Infinite loop to read and change volume
+        using a Pot and an ADC (mcp3008)."""
+
+        # this keeps track of the last potentiometer value
+        last_read = 0
+
+        while True:
+            # we'll assume that the pot didn't move
+            trim_pot_changed = False
+
+            # read the analog pin
+            trim_pot = self.chan0.value
+
+            # how much has it changed since the last read?
+            pot_adjust = abs(trim_pot - last_read)
+
+            if pot_adjust > self.tolerance:
+                trim_pot_changed = True
+
+            if trim_pot_changed:
+                # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
+                set_volume = self._remap_range(trim_pot, 0, 65535, 0, 100)
+
+                # set OS volume playback volume
+                print('Volume = {volume}%'.format(volume=set_volume))
+                set_vol_cmd = ('sudo amixer cset numid=1 -- {volume}% > /dev/null'.format(volume=set_volume))
+                system(set_vol_cmd)
+
+                # save the potentiometer reading for the next loop
+                last_read = trim_pot
+
+            # hang out and do nothing for a half second
+            sleep(0.5)
 
 
 class MailBoxLCD:
@@ -249,7 +329,9 @@ class Mechanics:
 # TODO: remove this when not testing pi
 
 if __name__ == "__main__":
-    m = Mechanics(22, 16, 20, 12, 6)
+    """m = Mechanics(22, 16, 20, 12, 6)
     while True:
         m.YouGotMail()
-        sleep(2)
+        sleep(2)"""
+    vol = VolumeControl()
+    vol.WatchVol()
